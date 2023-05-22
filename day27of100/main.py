64c82d5222cc5e4a96d44701fc8c56bac667e85a
Ahmetnarin <ahmetnarin568@gmail.com>
2023-05-22 12:35:25 +0300

    all changes

--- day27of100/main.py
+++ day27of100/main.py
@@ -1,4 +1,5 @@
 import tkinter
+import turtle
 
 window = tkinter.Tk()
 window.title("My first GUI program")
@@ -6,8 +7,11 @@ window.minsize(width=500, height=300)
 
 # Label 
 my_label = tkinter.Label(text="Hello!", font=("Arial", 24 , "bold"))
-my_label.pack()
+my_label.pack(side="left")
 
+tim = turtle.Turtle()
+tim.write("Some Text", font=("Arial", 20) )
+tim.hideturtle()
 
 
 
