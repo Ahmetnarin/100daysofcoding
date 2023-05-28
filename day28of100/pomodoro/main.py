0f6b3b841dfaf1c3218fea429c85b285f49a5a78
Ahmet Narin <ahmetnarin568@gmail.com>
2023-05-28 12:12:35 +0300

    commit all changes

--- day28of100/pomodoro/main.py
+++ day28of100/pomodoro/main.py
@@ -13,31 +13,22 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def stat_timer():
+    countdown(5)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 import time 
-
-# def countdown(minutes):
-#     seconds = minutes * 60
-#     while seconds > 0:
-#         mins, secs = divmod(seconds, 60)
-#         timer = '{:02d}:{:02d}'.format(mins,secs)
-#         canvas.itemconfig(timer_text, text=timer)
-#         # print(type(timer))
-#         time.sleep(1)
-#         seconds -= 1
-
 
 def countdown(count):
     canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, countdown, count - 1)
-
-
+    elif count == 0:
+        window.after(enable_button)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
-
 window.config(padx=100,pady=50, bg=YELLOW)
 
 canvas = Canvas( width= 200, height = 224, bg=YELLOW, highlightthickness=0)
@@ -46,14 +37,12 @@ canvas.create_image(100,112, image = tomato_img)
 timer_text = canvas.create_text(103,130, text= "00:00", fill="white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1, row=1)
 
-countdown(50)
+# countdown(50)
 
 
 # Create a label for Timer 
 timer_label = Label(text="Timer", font=(FONT_NAME, 40 , "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
-
- 
 
 def reset_timer():
     pass 
@@ -61,25 +50,13 @@ def reset_timer():
 def period():
     pass 
 
-
 # Create a button for start 
-start_button = Button(text="Start",  highlightthickness=0).grid(column=0, row=3) #, command=pass ) , command=countdown(1)
+start_button = Button(text="Start", command=stat_timer, highlightthickness=0 ).grid(column=0, row=3) #, command=pass ) , command=countdown(1)
 
 # Create a button for reset 
 reset_button = Button(text="Reset", command=reset_timer,  highlightthickness=0).grid(column=3, row=3) #, command=pass )
 
 # create a check_mark 
 check_mark = Label(text="✔", font=(FONT_NAME, 24 , "bold"), fg=GREEN, bg=YELLOW).grid(column=1, row=4)
-
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
--- day28of100/pomodoro/pomodoro_timer.py
+++ day28of100/pomodoro/pomodoro_timer.py
@@ -1,14 +1,45 @@
 import time
 
-def countdown(minutes):
-    seconds = minutes * 60
-    while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")  # Print the timer on the same line
-        time.sleep(1)
-        seconds -= 1
+# def countdown(minutes):
+#     seconds = minutes * 60
+#     while seconds > 0:
+#         mins, secs = divmod(seconds, 60)
+#         timer = '{:02d}:{:02d}'.format(mins, secs)
+#         print(timer, end="\r")  # Print the timer on the same line
+#         time.sleep(1)
+#         seconds -= 1
 
-    print("Timer completed!")
+#     print("Timer completed!")
 
-countdown(25)
+# countdown(25)
+
+
+
+import tkinter as tk
+import time
+
+def enable_button():
+    button.config(state=tk.NORMAL)
+
+def button_click():
+    print("Button clicked!")
+
+# Create a Tkinter window
+window = tk.Tk()
+
+# Create a button (initially disabled)
+button = tk.Button(window, text="Click Me", command=button_click, state=tk.DISABLED)
+button.pack()
+
+# Define the desired waiting time in seconds
+waiting_time = 1 * 60  # 25 minutes
+
+# Calculate the end time by adding the waiting time to the current time
+end_time = time.time() + waiting_time
+
+# Start a timer to enable the button when the waiting time elapses
+window.after(int(waiting_time * 1000), enable_button)
+
+# Start the Tkinter event loop
+window.mainloop()
+
