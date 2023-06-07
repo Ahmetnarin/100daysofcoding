b381ad232ef6d67b724f166fe639480fb4299fd7
Ahmet Narin <ahmetnarin568@gmail.com>
2023-06-07 19:27:54 +0300

    update

---
+++ day29of100/main.py
@@ -0,0 +1,12 @@
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Generator")
+logo =  "C://Users//ahnarin//Desktop//AHNARIN//100_days_of_coding//day29of100//logo.png"
+# window.config(pad=20)
+
+
