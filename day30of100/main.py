37d60820d0719a4ab0d9e63a534bff0e7b831ab3
Ahmet Narin <ahmetnarin568@gmail.com>
2023-07-02 00:43:46 +0300

    update my github

--- day30of100/main.py
+++ day30of100/main.py
@@ -3,7 +3,19 @@
 
 try:
     file = open("a_file.txt")
+    a_dictionary = {"key":"value"}
+    print(a_dictionary["key"])
 except FileNotFoundError:
     file = open("a_file.txt" , "a")
     file.write("Something")
-    # print("There was an error")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+
+
+
+
